d7a5fcd2df6dd3ef8bad35dd3d471e182d00d61b
JamesTizanou <jamestizanou@gmail.com>
2024-01-28 20:24:31 -0500

    kjbjhb

--- 05- USA-Simulator/Shooting.h
+++ 05- USA-Simulator/Shooting.h
@@ -8,6 +8,28 @@ const int Y_CONSOLE = 750;
 
 const V2d_i window = { X_CONSOLE, Y_CONSOLE };
 
+double convertir_en_radians(int angle)
+{
+	double newAngle = static_cast<double>(angle);
+	double angleInRad = newAngle * 3.14159265 / 180.0000000;
+	return angleInRad;
+}
+
+//void draw_full_circle(V2d_i pos, int radius)
+//{
+//	draw_circle(pos, radius);
+//	for (int i = pos.x - radius; i <= pos.x + radius; i++)
+//	{
+//		for (int n = pos.y - radius; n <= pos.y + radius; n++)
+//		{
+//			if (n * n + i * i  (pos.x + radius) * (pos.x + radius))
+//			{
+//				draw_pix({ i,n });
+//			}
+//		}
+//	}
+//}
+
 class player
 {
 private:
@@ -38,6 +60,14 @@ public:
 			pos.x--;
 		}
 	}
+	V2d_i get_pos()
+	{
+		return pos;
+	}
+	V2d_i get_xy()
+	{
+		return xy;
+	}
 	void show_coordinates()
 	{
 		pencil(COLOR_WHITE);
@@ -54,10 +84,11 @@ public:
 class bullets
 {
 private:
-	V2d_i pos;
-	V2d_i posI;
-	int longueur = 7;
-	V2d_i xy = {2, longueur};
+	V2d_i pos = {50,50};
+	int longueur = 15;
+	V2d_i xy = {3, longueur};
+	bool firstShot = true;
+	vector<V2d_i> points;
 
 	void show()
 	{
@@ -67,6 +98,129 @@ private:
 public:
 	void shoot(V2d_i posInitiale)
 	{
-		pos = posInitiale;
+		if (firstShot)
+		{
+			pos = posInitiale;
+			firstShot = false;
+		}
+		pencil(rgb(255, 0, 0));
+		draw_full_rect(Rect({ pos,xy }));
+		pos.y--;
+	}
+	V2d_i get_xy()
+	{
+		return xy;
+	}
+	bool is_on_screen()
+	{
+		if (pos.y > 0 && pos.y < Y_CONSOLE)
+		{
+			return true;
+		}
+		return false;
+	}
+	vector<V2d_i> get_points()
+	{
+		for (int i = pos.x; i <= pos.x + xy.x; i++)
+		{
+			for (int n = pos.y; n <= pos.y + xy.y; n++)
+			{
+				points.push_back({i,n});
+			}
+		}
+		return points;
+	}
+	V2d_i get_pos()
+	{
+		return pos;
+	}
+};
+
+
+class balls
+{
+private:
+	V2d_i pos = { 500,500 };
+	V2d_i velocite = { 0,0 };
+	vector<V2d_i> points;
+	int rayon = 25;
+	int acceleration = 2;
+	int numero = rayon;
+
+	void destroy()
+	{
+		pos = { 100000000,100000000 };
+	}
+
+public:
+	V2d_i get_pos()
+	{
+		return pos;
+	}
+	void show()
+	{
+		pencil(rgb(255, 0, 255));
+		draw_circle(pos, rayon);
+	}
+	void show_number()
+	{
+		pencil(COLOR_WHITE);
+		string x = entier_en_chaine(numero);
+		draw_simple_text(x, pos, get_font(0));
+	}
+	void move()
+	{
+		velocite = 0;
+		if (key_held(SDL_SCANCODE_A) && pos.x - rayon > 0)
+		{
+			velocite.x = -1;
+		}
+		if (key_held(SDL_SCANCODE_D) && pos.x + rayon < X_CONSOLE)
+		{
+			velocite.x = 1;
+		}
+		if (key_held(SDL_SCANCODE_W) && pos.y - rayon > 0)
+		{
+			velocite.y = -1;
+		}
+		if (key_held(SDL_SCANCODE_S) && pos.y + rayon < Y_CONSOLE)
+		{
+			velocite.y = 1;
+		}
+		pos.x += velocite.x * acceleration;
+		pos.y += velocite.y * acceleration;
+	}
+	vector<V2d_i> get_points()
+	{
+		for (int i = 0; i < 360; i++)
+		{
+			int x = rayon * cos(convertir_en_radians(i)) + pos.x;
+			int y = rayon * sin(convertir_en_radians(i)) + pos.y;
+			points.push_back({ x,y });
+		}
+		return points;
+	}
+	void boom(vector<bullets>& balles)
+	{
+		for (int i = 0; i < balles.size(); i++)
+		{
+			int x = balles.at(i).get_pos().x;
+			int y = balles.at(i).get_pos().y;
+			if ( pow(x - pos.x,2) + pow(y - pos.y,2) <= rayon * rayon)
+			{
+				balles.erase(balles.begin() + i);
+				numero--;
+				if (numero == 0)
+				{
+					sound().playSound("boom.wav");
+					destroy();
+				}
+				else
+				{
+					sound().playSound("bop.wav");
+				}
+				break;
+			}
+		}
 	}
 };
--- 05- USA-Simulator/main.cpp
+++ 05- USA-Simulator/main.cpp
@@ -1,11 +1,15 @@
 #include "Shooting.h"
 
 int main()
-{
+{ 
+	// Site pour les sound effects: myinstants.com
+
 	set_window_size(window);
 	set_window_resizable();
 
 	player joueur;
+	vector<bullets> balles;
+	balls ballons;
 
 	while (run())
 	{
@@ -13,5 +17,23 @@ int main()
 		draw_clear();
 		joueur.show();
 		joueur.move();
+		if (mouse_left_pressed())
+		{
+			sound().playSound("piou.wav");
+;			balles.push_back(bullets());
+		}
+		for (int i = 0; i < balles.size(); i++)
+		{
+			if (!balles.at(i).is_on_screen())
+			{
+				balles.erase(balles.begin() + i);
+				continue;
+			}
+			balles.at(i).shoot({ joueur.get_pos().x + (joueur.get_xy().x / 2), joueur.get_pos().y - balles.at(i).get_xy().y});
+		}
+		ballons.boom(balles);
+		ballons.show();
+		ballons.move();
+		ballons.show_number();
 	}
 }
