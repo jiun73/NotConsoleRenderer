85e39ce5c4c301c7ccdd82b50b2dfca360d75b8f
jiun73 <6227790@cstj.qc.ca>
2024-02-29 15:58:37 -0500

    nfdjkgnfdjg

--- All/main_chatroom.cpp
+++ All/main_chatroom.cpp
@@ -9,131 +9,135 @@ enum MessageTypes
 	NAME_UPDATE
 };
 
-std::stringstream ss;
-map<size_t, string> usernames;
-GLUU::Compiled_ptr menu;
-Server* server;
-
-void set_chatroom()
+namespace CHATROOM 
 {
-	server = new Server();
-	ss << "Start!" << std::endl;
+	void chatroom_main()
+	{
+		std::stringstream ss;
+		map<size_t, string> usernames;
+		GLUU::Compiled_ptr menu;
+		Server server;
 
-	GLUU::import_function<void()>(":chat_host", [&]()
-		{
-			server->open_session([](Server& server)
-				{
-					auto& buffers = server.get_buffers();
+		ss << "Start!" << std::endl;
 
-					map<size_t, string> usernames;
-
-					for (auto& b : buffers)
+		GLUU::import_function<void()>(":chat_host", [&]()
+			{
+				server.open_session([](Server& server)
 					{
-						ReadBuffer& rb = b.second;
-						if (rb.has(MESSAGE_IN))
+						auto& buffers = server.get_buffers();
+
+						map<size_t, string> usernames;
+
+						for (auto& b : buffers)
 						{
-							string s;
-							rb.rdc(MESSAGE_IN, s);
-							std::cout << s << std::endl;
-
-							server.broadcast(MESSAGE_OUT).wrtc(s) << net::send;
-						}
-
-						if (rb.has(SET_NAME))
-						{
-							string username;
-							rb.rdc(SET_NAME, username);
-							std::cout << "Added user " << username;
-							usernames.emplace(b.first, username);
-
-							server.broadcast(NAME_UPDATE) << usernames.size();
-
-							for (auto& name : usernames)
+							ReadBuffer& rb = b.second;
+							if (rb.has(MESSAGE_IN))
 							{
-								server.broadcast(NAME_UPDATE) << name.first;
-								server.broadcast(NAME_UPDATE).wrtc(name.second);
+								string s;
+								rb.rdc(MESSAGE_IN, s);
+								std::cout << s << std::endl;
+
+								server.broadcast(MESSAGE_OUT).wrtc(s) << net::send;
 							}
 
-							server.broadcast(NAME_UPDATE) << net::send;
+							if (rb.has(SET_NAME))
+							{
+								string username;
+								rb.rdc(SET_NAME, username);
+								std::cout << "Added user " << username;
+								usernames.emplace(b.first, username);
+
+								server.broadcast(NAME_UPDATE) << usernames.size();
+
+								for (auto& name : usernames)
+								{
+									server.broadcast(NAME_UPDATE) << name.first;
+									server.broadcast(NAME_UPDATE).wrtc(name.second);
+								}
+
+								server.broadcast(NAME_UPDATE) << net::send;
+							}
 						}
-					}
-				});
+					});
 
-			p2p().join();
-			//string s = "Admin";
-			//p2p(SET_NAME).wrtc(s) << net::send;
+				p2p().join();
+				//string s = "Admin";
+				//p2p(SET_NAME).wrtc(s) << net::send;
 
-		});
+			});
 
-	GLUU::import_function<void()>(":chat_wait", [&]()
-		{
-			server->wait_for_peer();
-		});
-
-	GLUU::import_function<bool(string&, string&)>(":chat_join", [&](string& b, string& a)
-		{
-			bool success = p2p().join(a);
-			/*if (success)
+		GLUU::import_function<void()>(":chat_wait", [&]()
 			{
-				p2p(SET_NAME).wrtc(b) << net::send;
-			}*/
-			return success;
-		});
+				server.wait_for_peer();
+			});
 
-	GLUU::import_function<void()>(":chat_disconnect", [&]()
+		GLUU::import_function<bool(string&, string&)>(":chat_join", [&](string& b, string& a)
+			{
+				bool success = p2p().join(a);
+				/*if (success)
+				{
+					p2p(SET_NAME).wrtc(b) << net::send;
+				}*/
+				return success;
+			});
+
+		GLUU::import_function<void()>(":chat_disconnect", [&]()
+			{
+
+			});
+
+		GLUU::import_function<string()>(":chat_msg", [&]()
+			{
+				return ss.str();
+			});
+
+		GLUU::import_function<int()>(":peercnt", [&]()
+			{
+				return server.peer_count();
+			});
+
+		GLUU::import_function<void(string&)>(":chat_send", [&](string& s)
+			{
+				p2p(MESSAGE_IN).wrtc(s) << net::send;
+			});
+
+		menu = GLUU::parse_file("GLUU/chat_menu.gluu");
+
+		while (run())
 		{
+			pencil(COLOR_BLACK);
+			draw_clear();
 
-		});
+			menu->render({ 0,get_logical_size() });
 
-	GLUU::import_function<string()>(":chat_msg", [&]()
-		{
-			return ss.str();
-		});
+			if (p2p().has_stream(MESSAGE_OUT))
+			{
+				string s;
+				p2p(MESSAGE_OUT).rdc(s);
 
-	GLUU::import_function<int()>(":peercnt", [&]()
-		{
-			return server->peer_count();
-		});
+				ss << s << std::endl;
+			}
 
-	GLUU::import_function<void(string&)>(":chat_send", [&](string& s)
-		{
-			p2p(MESSAGE_IN).wrtc(s) << net::send;
-		});
+			if (p2p().has_stream(NAME_UPDATE))
+			{
+				usernames.clear();
 
-	menu = GLUU::parse_file("GLUU/chat_menu.gluu");
-}
-
-int update_chatroom()
-{
-	pencil(COLOR_BLACK);
-	draw_clear();
-
-	menu->render({ 0,get_logical_size() });
-
-	if (p2p().has_stream(MESSAGE_OUT))
-	{
-		string s;
-		p2p(MESSAGE_OUT).rdc(s);
-
-		ss << s << std::endl;
-	}
-
-	if (p2p().has_stream(NAME_UPDATE))
-	{
-		usernames.clear();
-
-		size_t sz = p2p().read<size_t>(NAME_UPDATE);
-		for (size_t i = 0; i < sz; i++)
-		{
-			size_t id = p2p().read<size_t>(NAME_UPDATE);
-			string s;
-			p2p().rdc(s);
-			usernames.emplace(id, s);
+				size_t sz = p2p().read<size_t>(NAME_UPDATE);
+				for (size_t i = 0; i < sz; i++)
+				{
+					size_t id = p2p().read<size_t>(NAME_UPDATE);
+					string s;
+					p2p().rdc(s);
+					usernames.emplace(id, s);
+				}
+			}
 		}
 	}
-
-	return 1;
 }
 
- GLUU::ImportFunction<decltype(set_chatroom)> gluu_set_chatroom_import("$" + string("set_chatroom"), set_chatroom);
- GLUU::ImportFunction<decltype(update_chatroom)> gluu_update_chatroom_import("$" + string("update_chatroom"), update_chatroom);
+
+#include "GLUU.h"
+
+namespace CHATROOM {
+	GLUU_IMPORT_MAIN(chatroom_main);
+}
