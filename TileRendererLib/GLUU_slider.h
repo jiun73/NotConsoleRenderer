941156fc39c619ea76d32c4715132dfb357ea8f7
jiun73 <6227790@cstj.qc.ca>
2024-04-09 13:53:02 -0400

    started slider

---
+++ TileRendererLib/GLUU_slider.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "GLUU_parser.h"
+
+namespace GLUU {
+	class ButtonWidget : public Widget
+	{
+		GLUU_Make(2, "slider")
+		{
+			auto ptr = make_shared<ButtonWidget>();
+
+			ptr->min.set(args[0], parser);
+			ptr->max.set(args[1], parser);
+
+			return ptr;
+		}
+
+		SeqVar<int> max;
+		SeqVar<int> min;
+
+		bool lock = false;
+
+		void update(Element& graphic) override
+		{
+			
+		}
+	};
+}
